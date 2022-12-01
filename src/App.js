e81c14721ac24da2452b144324bcee5ac250631c
Mike Oram <Mikeoram01@gmail.com>
2022-12-01 11:24:15 +0000

    Created fetch and advice header componenet

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import './App.css';
+import Container from "./Components/Container/Container";
+import AdviceHeader from "./Components/AdviceHeader/AdviceHeader";
+import {useEffect, useState} from "react";
 
 function App() {
+    const [advice, setAdvice] = useState();
+
+  const fetchAdviceFromApi = async () => {
+      const advice = await fetch('https://api.adviceslip.com/advice');
+      const adviceJson = await advice.json();
+      setAdvice(adviceJson.slip);
+  }
+
+  useEffect(() => {
+      fetchAdviceFromApi()
+  }, [])
+
   return (
     <div className="App">
+      <Container className={"quote-container"}>
+          {!advice ? "" :
+              <AdviceHeader className={"advice-header"} adviceId={advice.id}/>
+          }
+      </Container>
     </div>
   );
 }
---
+++ src/Components/AdviceHeader/AdviceHeader.js
@@ -0,0 +1,7 @@
+const AdviceHeader = (props) => {
+    return (
+        <h1 className={props.className}>ADVICE #{props.adviceId}</h1>
+    )
+}
+
+export default AdviceHeader
