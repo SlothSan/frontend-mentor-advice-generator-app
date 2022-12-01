22699c111ee9ec37fa3bc4cdbd5f9587512630d5
Mike Oram <Mikeoram01@gmail.com>
2022-12-01 11:42:36 +0000

    Created the divider component and styled it

--- src/App.js
+++ src/App.js
@@ -2,29 +2,35 @@ import './App.css';
 import Container from "./Components/Container/Container";
 import AdviceHeader from "./Components/AdviceHeader/AdviceHeader";
 import {useEffect, useState} from "react";
+import AdviceText from "./Components/AdviceText/AdviceText";
+import Divider from "./Components/Divider/Divider";
 
 function App() {
     const [advice, setAdvice] = useState();
 
-  const fetchAdviceFromApi = async () => {
-      const advice = await fetch('https://api.adviceslip.com/advice');
-      const adviceJson = await advice.json();
-      setAdvice(adviceJson.slip);
-  }
+    const fetchAdviceFromApi = async () => {
+        const advice = await fetch('https://api.adviceslip.com/advice');
+        const adviceJson = await advice.json();
+        setAdvice(adviceJson.slip);
+    }
 
-  useEffect(() => {
-      fetchAdviceFromApi()
-  }, [])
+    useEffect(() => {
+        fetchAdviceFromApi()
+    }, [])
 
-  return (
-    <div className="App">
-      <Container className={"quote-container"}>
-          {!advice ? "" :
-              <AdviceHeader className={"advice-header"} adviceId={advice.id}/>
-          }
-      </Container>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Container className={"quote-container"}>
+                {!advice ? "" :
+                    <>
+                        <AdviceHeader className={"advice-header"} adviceId={advice.id}/>
+                        <AdviceText className={"advice-text"} advice={advice.advice}/>
+                        <Divider className={"mobile-divider"}/>
+                    </>
+                }
+            </Container>
+        </div>
+    );
 }
 
 export default App;
